e9e3de17b469c5a03dccd126aba31b9b3f1254c0
CarvalhoGabriel <gabriel.f.ext@tooseguros.com.br>
2023-02-26 18:29:26 -0300

    Importando modulo de geração de report

--- Projetct_K6/MaoNaMassa/exec1_smokeTest.js
+++ Projetct_K6/MaoNaMassa/exec1_smokeTest.js
@@ -1,5 +1,6 @@
 import * as http from 'k6/http'
 import { check } from 'k6'
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 
 export const options= {
