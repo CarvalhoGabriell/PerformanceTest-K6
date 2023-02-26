1edca7e4417257754b85782fe33e64386a596782
CarvalhoGabriel <gabriel.f.ext@tooseguros.com.br>
2023-02-26 19:28:58 -0300

    Atualizando path de geração de report

--- Projetct_K6/MaoNaMassa/exec2_LoadTest_GetUsers.js
+++ Projetct_K6/MaoNaMassa/exec2_LoadTest_GetUsers.js
@@ -42,6 +42,6 @@ export default function() {
 // no metodo de retorno informar o caminho que o report deve ser armazenado e o nome do arquivo terminado em .html
 export function handleSummary(data) {
     return {
-      "../logs/index.html": htmlReport(data),
+      "index.html": htmlReport(data),
     };
   }
